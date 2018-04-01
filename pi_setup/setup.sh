e81461042ba90be16481b3d535d421e4abb43236
WisdomWolf <wisdomwolf@gmail.com>
2018-04-01 01:19:26 +0000

    added pi_setup.sh

---
+++ pi_setup/setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+[ "$UID" -eq 0 ] || exec sudo -E "$0" "$@"
+echo "Home: $HOME"
+if ! [ -x "$(command -v node)" ]; then
+	curl -sL https://deb.nodesource.com/setup_8.x | bash -
+else
+	echo "skipping node install"
+fi
+apt update
+apt install -y fish tmux vim curl wget git httpie jq sed nodejs
+if ! [ -e $HOME/.config/fish/functions/fisher.fish ]; then
+	echo "Downloading fisher"
+	curl -Lo $HOME/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher
+	chown -R $SUDO_USER:$SUDO_USER $HOME/.config/fish/functions
+else
+	echo "skipping fisher install"
+fi
+if ! [ -x "$(command -v yadm)" ]; then
+	echo "Downloading yadm"
+	curl -fLo /usr/local/bin/yadm https://github.com/TheLocehiliosan/yadm/raw/master/yadm && chmod a+x /usr/local/bin/yadm
+else
+	echo "skipping yadm install"
+fi
