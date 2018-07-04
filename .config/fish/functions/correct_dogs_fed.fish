6bd909be15ec5712603684a79a592e845120a342
WisdomWolf <wisdomwolf@gmail.com>
2018-07-04 14:28:36 -0500

    added default functionality to correct_dogs_fed

--- .config/fish/functions/correct_dogs_fed.fish
+++ .config/fish/functions/correct_dogs_fed.fish
@@ -1,8 +1,14 @@
-# Defined in /tmp/fish.opVdU6/correct_dogs_fed.fish @ line 2
+# Defined in /tmp/fish.9pZqGL/correct_dogs_fed.fish @ line 2
 function correct_dogs_fed
-	set date $argv[1]
-if test -z "$MQTT_PASS"
-    read -i -P "mqtt pass: " MQTT_PASS
-end
-mosquitto_pub -h mqtt.developingwisdom.org -t iot/sensors/dog-food -m "{\"last_fed\": \"$date\"}" -p 8883 --capath /etc/ssl/certs/ -u "wisdomwolf" -P $MQTT_PASS
+	if test (count $argv) -gt 0
+        set date $argv[1]
+    else
+        echo 'setting date to default'
+        set date (bash -c "TZ='America/New_York' date \"+%Y/%m/%d %I:%M%p\"")
+    end
+    if test -z "$MQTT_PASS"
+        read -i -P "mqtt pass: " MQTT_PASS
+    end
+    echo "correcting last fed to $date"
+    mosquitto_pub -h mqtt.developingwisdom.org -t iot/sensors/dog-food -m "{\"last_fed\": \"$date\"}" -p 8883 --capath /etc/ssl/certs/ -u "wisdomwolf" -P $MQTT_PASS
 end
