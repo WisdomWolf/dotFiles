27c1791caebbe3d284b0251209c2d21b36f335f3
WisdomWolf <wisdomwolf@gmail.com>
2018-07-04 14:30:12 -0500

    adjusted column order for dps command

--- .config/fish/functions/dps.fish
+++ .config/fish/functions/dps.fish
@@ -1,4 +1,4 @@
-# Defined in - @ line 0
+# Defined in /tmp/fish.RF7RPl/dps.fish @ line 2
 function dps --description alias\ dps=docker\ ps\ --format\ \"table\ \{\{.Image\}\}\\t\{\{.Names\}\}\\t\{\{.CreatedAt\}\}\\t\{\{.Status\}\}\"
-	docker ps --format "table {{.Image}}\t{{.Names}}\t{{.CreatedAt}}\t{{.Status}}" $argv;
+	docker ps --format "table {{.Names}}\t{{.Image}}\t{{.CreatedAt}}\t{{.Status}}" $argv;
 end
