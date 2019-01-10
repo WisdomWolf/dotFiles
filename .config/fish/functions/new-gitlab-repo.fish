303a034390ef384d546ad4a042c063874a6b8bae
WisdomWolf <wisdomwolf@gmail.com>
2019-01-10 10:31:26 -0500

    updated new-gitlab-repo to work with older fish versions

--- .config/fish/functions/new-gitlab-repo.fish
+++ .config/fish/functions/new-gitlab-repo.fish
@@ -1,9 +1,14 @@
-# Defined in /tmp/fish.A2X5aF/new-gitlab-repo.fish @ line 3
 function new-gitlab-repo
-	set -l new_project (curl --header "PRIVATE-TOKEN: $GITLAB_TOKEN" -s https://gitlab.com/api/v4/projects -d name=$argv[1])
+	if test (count $argv) -lt 1
+        set project_name (string split '/' (pwd))[-1]
+    else
+        set project_name $argv[1]
+    end
+    echo "DEBUG: project_name = $project_name"
+    set -l new_project (curl --header "PRIVATE-TOKEN: $GITLAB_TOKEN" -s https://gitlab.com/api/v4/projects -d name=$project_name)
     set -l ssh_url (echo $new_project | jq -r '.ssh_url_to_repo')
     echo $new_project | jq -r '"\(.id)\t\(.name)\t\(.web_url)"'
-    read -P "Would you like to add this project as remote git repo? " -n 1 -l add_git_repo
+    read -p 'echo "Would you like to add this project as remote git repo? "' -n 1 -l add_git_repo
     if test "$add_git_repo" = "y" -o "$add_git_repo" = "Y"
         git remote add origin $ssh_url
         echo "remote repo set"
