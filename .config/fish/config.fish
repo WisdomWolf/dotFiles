966e35830be2228902a68a2dde5cd7fcdf042a86
WisdomWolf <wisdomwolf@gmail.com>
2020-07-26 13:06:28 -0400

    updates alternate files for yadm 2.0

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -19,7 +19,29 @@ set -x SHELL (which fish)
 setenv SSH_ENV $HOME/.ssh/environment
 
 if test -n "$WSL_DISTRO_NAME"
-    eval (/mnt/c/ProgramData/weasel-pageant/weasel-pageant -S fish -rb -a $HOME/.weasel-pageant.sock)
+    set -x SSH_AUTH_SOCK $HOME/.ssh/agent.sock
+    ss -a | grep -q $SSH_AUTH_SOCK
+    if [ $status != 0 ]
+        rm -f $SSH_AUTH_SOCK
+        setsid nohup socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:$HOME/.ssh/wsl2-ssh-pageant.exe >/dev/null 2>&1 &
+    end
+else
+    if [ -n "$SSH_AGENT_PID" ]
+        ps -ef | grep $SSH_AGENT_PID | grep ssh-agent >/dev/null
+        if [ $status -eq 0 ]
+            test_identities
+        end
+    else
+        if [ -f $SSH_ENV ]
+            source $SSH_ENV >/dev/null
+        end
+        ps -ef | grep $SSH_AGENT_PID | grep -v grep | grep ssh-agent >/dev/null
+        if [ $status -eq 0 ]
+            test_identities
+        else
+            start_agent
+        end
+    end
 end
 
 function start_agent
@@ -41,22 +63,6 @@ function test_identities
     end
 end
 
-if [ -n "$SSH_AGENT_PID" ]
-    ps -ef | grep $SSH_AGENT_PID | grep ssh-agent >/dev/null
-    if [ $status -eq 0 ]
-        test_identities
-    end
-else
-    if [ -f $SSH_ENV ]
-        source $SSH_ENV >/dev/null
-    end
-    ps -ef | grep $SSH_AGENT_PID | grep -v grep | grep ssh-agent >/dev/null
-    if [ $status -eq 0 ]
-        test_identities
-    else
-        start_agent
-    end
-end
 
 if test -e $HOME/.pyenv/bin/pyenv
     if not contains "$HOME/.pyenv/bin" $fish_user_paths
@@ -65,7 +71,7 @@ if test -e $HOME/.pyenv/bin/pyenv
     status --is-interactive; and source (pyenv init -|psub)
     status --is-interactive; and source (pyenv virtualenv-init -|psub)
 end
-source ~/.config/extraterm/commands/setup_extraterm_fish.fish
+# source ~/.config/extraterm/commands/setup_extraterm_fish.fish
 
 if not functions -q fisher
     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
