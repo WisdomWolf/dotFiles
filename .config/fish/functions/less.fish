74037be434885b944db100e83bf21fa2db66e838
WisdomWolf <wisdomwolf@gmail.com>
2020-12-01 10:17:02 -0500

    adds sensible defaults to less

---
+++ .config/fish/functions/less.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function less --wraps='less -Xr --quit-if-one-screen' --description 'alias less=less -Xr --quit-if-one-screen'
+ command less -Xr --quit-if-one-screen $argv;
+end
