a6431a3387991746d679b57a124ac69ba756687f
WisdomWolf <wisdomwolf@gmail.com>
2021-07-09 16:16:27 -0400

    updates upgrade-venv to reduce failures

--- .config/fish/functions/upgrade-venv.fish
+++ .config/fish/functions/upgrade-venv.fish
@@ -1,11 +1,16 @@
+# Defined in /tmp/fish.GbEFzJ/upgrade-venv.fish @ line 2
 function upgrade-venv
-	set $new_version $argv[1]
-set venv_name (pyenv version-name)
-pip freeze > .pyenv_requirements.txt
-pyenv uninstall $venv_name
-pyenv virtualenv $new_version $venv_name
-pyenv local $venv_name
-pip install -U pip
-pip install -r .pyenv_requirements.txt
-echo "upgrade to $new_version completed successfully."
+	set new_version $argv[1]
+    if [ -z "$new_version" ]
+        echo 'Usage: upgrade-venv <new_version>'
+        return 1
+    end
+    set venv_name (pyenv version-name)
+    pip freeze > .pyenv_requirements.txt
+    pyenv uninstall $venv_name
+    pyenv virtualenv $new_version $venv_name
+    pyenv local $venv_name
+    pip install -U pip
+    pip install -r .pyenv_requirements.txt
+    echo "upgrade to $new_version completed successfully."
 end
