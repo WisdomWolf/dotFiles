d6aa60a0c8e3274bf82fc645424de4915b0bc099
WisdomWolf <wisdomwolf@gmail.com>
2021-09-10 12:49:53 -0400

    updates config.fish with pyenv fixes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -39,31 +39,30 @@ function test_identities
 end
 
 if test -n "$WSL_DISTRO_NAME"
-    set -x SSH_AUTH_SOCK $HOME/.ssh/agent.sock
-    ss -a | grep -q $SSH_AUTH_SOCK
-    if [ $status != 0 ]
-        rm -f $SSH_AUTH_SOCK
-        setsid nohup socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:$HOME/.ssh/wsl2-ssh-pageant.exe >/dev/null 2>&1 &
+#     set -x SSH_AUTH_SOCK $HOME/.ssh/agent.sock
+#     ss -a | grep -q $SSH_AUTH_SOCK
+#     if [ $status != 0 ]
+#         rm -f $SSH_AUTH_SOCK
+#         setsid nohup socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:$HOME/.ssh/wsl2-ssh-pageant.exe >/dev/null 2>&1 &
+#     end
+    set -Ux DISPLAY (cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}'):0.0
+end
+if [ -n "$SSH_AGENT_PID" ]
+    ps -ef | grep $SSH_AGENT_PID | grep ssh-agent >/dev/null
+    if [ $status -eq 0 ]
+        test_identities
     end
 else
-    if [ -n "$SSH_AGENT_PID" ]
-        ps -ef | grep $SSH_AGENT_PID | grep ssh-agent >/dev/null
-        if [ $status -eq 0 ]
-            test_identities
-        end
+    if [ -f $SSH_ENV ]
+        source $SSH_ENV >/dev/null
+    end
+    ps -ef | grep $SSH_AGENT_PID | grep -v grep | grep ssh-agent >/dev/null
+    if [ $status -eq 0 ]
+        test_identities
     else
-        if [ -f $SSH_ENV ]
-            source $SSH_ENV >/dev/null
-        end
-        ps -ef | grep $SSH_AGENT_PID | grep -v grep | grep ssh-agent >/dev/null
-        if [ $status -eq 0 ]
-            test_identities
-        else
-            start_agent
-        end
+        start_agent
     end
 end
-
 
 
 function test_identities
@@ -81,13 +80,27 @@ if test -e $HOME/.pyenv/bin/pyenv
     if not contains "$HOME/.pyenv/bin" $fish_user_paths
         set -x fish_user_paths $HOME/.pyenv/bin $fish_user_paths
     end
-    status --is-interactive; and source (pyenv init -|psub)
+    status --is-interactive; and source (pyenv init --path | source)
+    status --is-interactive; and pyenv init - | source
     status --is-interactive; and source (pyenv virtualenv-init -|psub)
+
+    #pyenv-virtualenv init
+    set -ga fish_user_paths '/usr/local/Cellar/pyenv-virtualenv/1.1.5/shims'
+    set -gx PYENV_VIRTUALENV_INIT 1;
+    function _pyenv_virtualenv_hook --on-event fish_prompt
+    set -l ret $status
+    if [ -n "$VIRTUAL_ENV" ]
+        pyenv activate --quiet; or pyenv deactivate --quiet; or true
+    else
+        pyenv activate --quiet; or true
+    end
+    return $ret
+    end
 end
 # source ~/.config/extraterm/commands/setup_extraterm_fish.fish
 
-if not functions -q fisher
-    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-    fish -c fisher
-end
+#if not functions -q fisher
+#    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+#    curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+#end
+starship init fish | source
