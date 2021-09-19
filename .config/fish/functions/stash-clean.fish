8c27146c9233b5c69b650225abc28984e7504bdb
WisdomWolf <wisdomwolf@gmail.com>
2021-09-19 11:52:31 -0400

    updates stash-clean

--- .config/fish/functions/stash-clean.fish
+++ .config/fish/functions/stash-clean.fish
@@ -1,3 +1,4 @@
+# Defined in /tmp/fish.OParEm/stash-clean.fish @ line 2
 function stash-clean
-stash-api --data-binary '{"query":"mutation { metadataClean() }"}'
+    stash-api --data-binary '{"query":"mutation { metadataClean( input: { dryRun: false} ) }"}'
 end
