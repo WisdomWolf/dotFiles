0d5e8c4504445bd2a7cc12a582a5b43f9e5590ab
WisdomWolf <wisdomwolf@gmail.com>
2022-01-04 16:44:11 -0500

    Fixes stupidity on code function

--- .config/fish/functions/code.fish
+++ .config/fish/functions/code.fish
@@ -1,8 +1,9 @@
+# Defined in /tmp/fish.AGphYl/code.fish @ line 2
 function code
     if test (command -s code-oss)
         code-oss $argv
     else if test (command -s code)
-        code $argv
+        command code $argv
     else
         echo "Code doesn't appear to be installed"
     end
