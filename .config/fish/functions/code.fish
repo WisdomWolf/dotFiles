97c25cd51de82e6da485f18ffac17015d3aa2d56
WisdomWolf <wisdomwolf@gmail.com>
2022-05-22 15:26:21 -0400

    Updates code function with more command options

--- .config/fish/functions/code.fish
+++ .config/fish/functions/code.fish
@@ -1,6 +1,8 @@
 # Defined in /tmp/fish.AGphYl/code.fish @ line 2
 function code
-    if test (command -s code-oss)
+    if test (command -s code-insiders)
+        code-insiders $argv
+    else if test (command -s code-oss)
         code-oss $argv
     else if test (command -s code)
         command code $argv
