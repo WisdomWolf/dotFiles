47c5ed049e451ac7c39893aa73d844914d0e5984
WisdomWolf <wisdomwolf@gmail.com>
2023-02-16 14:41:57 -0500

    sets ipython colors to monokai

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -604,4 +604,4 @@ c.InteractiveShellApp.exec_lines.append('%autoreload 2')
 c.InteractiveShellApp.exec_lines.append('print("Warning: disable autoreload in ipython_config.py to improve performance.")')
 c.InteractiveShellApp.exec_lines.append(
     "try:\n    %load_ext ipython_autoimport\nexcept ImportError: pass")
-
+c.TerminalInteractiveShell.highlighting_style = 'monokai'
