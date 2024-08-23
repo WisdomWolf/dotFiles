8a73133cab0394404b9e1a9cf30dea36507490e4
WisdomWolf <wisdomwolf@gmail.com>
2024-08-23 10:50:17 -0400

    refines photo grouping function

--- .config/fish/functions/group-photos-by-month.fish
+++ .config/fish/functions/group-photos-by-month.fish
@@ -1,8 +1,13 @@
 function group-photos-by-month
-for file in (ls *.JPG)
-set date_str (echo "$file" | cut -d_ -f1)
-set date_dir (date --date="$date_str" "+%Y-%m")
-mkdir -pv "$date_dir"
-mv -v -- "$file" "$date_dir/"
-end
+    if test -z "$argv"
+        set search_str *.JPG
+    else
+        set search_str $argv
+    end
+    for file in (ls $search_str)
+        set date_str (echo "$file" | cut -d_ -f1)
+        set date_dir (date --date="$date_str" "+%Y-%m")
+        mkdir -pv "$date_dir"
+        mv -v -- "$file" "$date_dir/"
+    end
 end
